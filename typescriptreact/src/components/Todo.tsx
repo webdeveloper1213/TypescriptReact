7eea16eceafa3730b61b8306df0127c4e23c25cd
webdeveloper1213 <sameera_sulaiman@yahoo.com>
2022-12-21 15:20:21 -0800

    Added TODO

--- typescriptreact/src/components/Todo.tsx
+++ typescriptreact/src/components/Todo.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Todo = () => {
+    const [todo , setTodo] = useState("");
+    const [todoList , setTodoList] = useState<string[]>([]);
+    const [remove , setRemove] = useState(false);
+
+    const changeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setTodo(e.target.value);
+        
+    }
+    const addHandler = () => {
+    setTodoList([...todoList , todo]);
+    setTodo("");
+    }
+    const removeHandler = (index:any) => {
+        console.log("Remove button clicked")
+      const filteredList = todoList.filter((todoIndex) => todoIndex !== index);
+     const ans = setTodoList(filteredList);
+     console.log(ans,"filtered List");
+    }
+
   return (
+    <>
     <div>Todo</div>
+    <div>
+    <input 
+    type="text"
+    value={todo}
+    onChange = {changeHandler}
+    />
+    <button onClick={addHandler}>Add</button>
+<ul>
+{todoList.map((item,index) => (
+    <>
+    <div style={{display:'flex',gap:'0.5rem'}} >
+    <li key={index}>{item}</li>
+        <button onClick={() =>removeHandler(item)}>Remove</button>
+    </div>
+       
+        </>
+    ))}
+</ul>
+  </div>
+    </>
+    
   )
 }
 
